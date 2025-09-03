cf4064b91f6b6665d047b4bc5b4c3f3d54ea4a25
RAFAEL DO NASCIMENTO SANTOS <rafael.nsantos18@senacsp.edu.br>
2025-09-03 12:00:01 -0300

    [FAET] - Aula concluída

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import './App.css'
 import Navbar from './components/navbar/Navbar'
-import Carousel from './components/carousel/Carousel'
-import CategoryCard from './components/categoryCard/CategoryCard'
 import CategoryCarousel from './components/categoryCarousel/categoryCarousel'
+import AnimalCarousel from './components/animalCarousel/AnimalCarousel'
+import Hero from './components/hero/Hero'
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <Hero />
       <CategoryCarousel />
+      <AnimalCarousel />
     </>
   )
 }
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,31 @@
+import HeroImg from '../../assets/heroImg.svg'
+import './Hero.css'
+import CTAButton from '../ctaBtn/CTABtn'
+
+const Hero = () => {
+  const heroList = {
+    name: 'Pet shop name',
+    title: 'bem vindo',
+    image: HeroImg,
+    titleImg: 'img title',
+    subtitle: 'subtitle',
+    buttonHeroText: 'comprar',
+    buttonHeroLink: '#'
+  };
+  
+    return(
+        <section className='hero'>
+            <div className='hero-content'>
+                <span className='subtitle'>Pet Shop</span>
+                <h1>{heroList.title}</h1>
+                <p>lorem djandasdasndasas sdiajhajoif asnfiasfjasfjfi</p>
+                <CTAButton text="Comprar agora"/>
+            </div>
+            <div className='hero-image-container '>
+                <img className='img-hero' src={HeroImg} alt="hero img" />
+            </div>
+        </section>
+    )
+}
+
+export default Hero;
