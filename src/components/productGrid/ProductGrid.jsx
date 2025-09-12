5af206465b93f5eaddde4556f002afb0c5963a48
RAFAEL DO NASCIMENTO SANTOS <rafael.nsantos18@senacsp.edu.br>
2025-09-12 11:50:28 -0300

    Aula concluída

--- src/components/productGrid/ProductGrid.jsx
+++ src/components/productGrid/ProductGrid.jsx
@@ -1,41 +1,9 @@
 import ShopSidebar from '../shopSidebar/ShopSidebar';
 import ProductCard from '../productCard/ProductCard';
 import './ProductGrid.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Pagination from '../pagination/Pagination';
-
-const todosProdutos = [
-  { id: 1, name: "Camiseta", price: 29.99 },
-  { id: 2, name: "Calça Jeans", price: 79.99 },
-  { id: 3, name: "Tênis", price: 120.00 },
-  { id: 4, name: "Boné", price: 19.99 },
-  { id: 5, name: "Mochila", price: 150.50 },
-  { id: 6, name: "Relógio", price: 250.00 },
-  { id: 7, name: "Óculos de Sol", price: 180.75 },
-  { id: 8, name: "Jaqueta", price: 300.00 },
-  { id: 9, name: "Meias", price: 12.50 },
-  { id: 10, name: "Tênis de Corrida", price: 130.00 },
-  { id: 11, name: "Camisa Social", price: 45.00 },
-  { id: 12, name: "Cinto", price: 35.00 },
-  { id: 13, name: "Carteira", price: 60.00 },
-  { id: 14, name: "Luvas", price: 25.00 },
-  { id: 15, name: "Gorro", price: 20.00 },
-  { id: 16, name: "Blusa de Frio", price: 100.00 },
-  { id: 17, name: "Shorts", price: 40.00 },
-  { id: 18, name: "Chinelo", price: 15.00 },
-  { id: 19, name: "Sandália", price: 50.00 },
-  { id: 20, name: "Bolsa", price: 200.00 },
-  { id: 21, name: "Pulseira", price: 45.50 },
-  { id: 22, name: "Brinco", price: 60.00 },
-  { id: 23, name: "Colar", price: 80.00 },
-  { id: 24, name: "Camiseta Regata", price: 25.00 },
-  { id: 25, name: "Calça Legging", price: 70.00 },
-  { id: 26, name: "Jaqueta Corta Vento", price: 150.00 },
-  { id: 27, name: "Moletom", price: 90.00 },
-  { id: 28, name: "Tênis Casual", price: 110.00 },
-  { id: 29, name: "Relógio Digital", price: 300.00 },
-  { id: 30, name: "Carteira Masculina", price: 55.00 }
-];
+import axios from 'axios'
 
 const listaDeCategorias = [
   { nome: "Brinquedos", qtd: 32 }, // índice 0
@@ -51,7 +19,65 @@ const listaDeMarcas = [
 
 const ProductGrid = () => {
     const [paginaAtual, setPaginaAtual] = useState(1);
+    const [loading, setLoading] = useState(false);
+    const [pets, setPets] = useState([])
     const produtosPorPagina = 6;
+    const maximoDeAnimais = 50;
+
+    const API_KEY = "fNTK51JOa6lJfT9PmVAILwhIbwJOs1tQ5Aq6T67hVGdfbpLqmq";
+    const API_SECRET = "lnmIzDozp8MEi7lciTbPQth1Hx1YdezTUXnzSaNv";
+
+    const obterToken = async() => {
+        const response = await axios.post(
+            "https://api.petfinder.com/v2/oauth2/token",
+            `grant_type=client_credentials&client_id=${API_KEY}&client_secret=${API_SECRET}`,
+            {
+                headers: { "Content-Type": "aplication/x-www-form-urlencoded" }
+            }
+        );
+        return response.data.access_token;
+    }
+
+    const buscarPets = async (token, pagina, limite) => {
+        const response = await axios.get(`/pf-api-/animals?page=${pagina}&limit=${limite}&type=Dog`,
+            {
+                headers: { Authorization: `Berear ${token}` }
+            }
+        )
+        return response.data;
+    }
+
+    useEffect(()=> {
+        const fetchTokenEPets = async () => {
+            if(loading);
+            setLoading(true);
+
+            try {
+                const token = await obterToken();
+                const data = await buscarPets(token, paginaAtual, produtosPorPagina);
+
+                const petsComImagem = data.animals.filter(
+                    (pet) => pet.primary_photo_cropped || (pet.photos.length > 0)
+                )
+
+                const petsFormatados = petsComImagem.map((cachorro)=> ({
+                    id: cachorro.id,
+                    name: cachorro.name,
+                    price: cachorro.breeds.primary,
+                    image: cachorro.primary_photo_cropped?.medium || (cachorro.photo && photos[0]?.medium)
+                }));
+
+                setPets(petsFormatados)
+
+                const totalConsiderado = Math.min(
+                    maximoDeAnimais,
+                    data.pagination.total_count
+                )
+            } catch {
+
+            }
+        }
+    })
 
     // ceil serve para arrendodar pra cima
     const totalDePaginas = Math.ceil(todosProdutos.length / produtosPorPagina);
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: { 
+    proxy: {
+      '/pf/-api': {
+        target: 'https://api.petfinder.com/v2',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/pf-api/, '')
+      } 
+    }
+  }
 })
